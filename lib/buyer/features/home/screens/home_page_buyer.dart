33230ed4ddd12ed57c37bcdd7acd837d8464a9c1
RayhanLauzzadani <rlauzzadani@gmail.com>
2025-07-05 16:41:02 +0700

    refactor(buyer): restructure folder and file organization for buyer module

--- lib/buyer/features/home/screens/home_page_buyer.dart
+++ lib/buyer/features/home/screens/home_page_buyer.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import '../widgets/search_bar.dart' as custom;
-import '../widgets/banner/promo_banner_carousel.dart'; // Cukup carousel saja
+import '../../../widgets/search_bar.dart' as custom;
+import '../../auth/widgets/banner/promo_banner_carousel.dart'; // Cukup carousel saja
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
