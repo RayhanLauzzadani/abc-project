34488833688ce11a244448bd995eeeae181d0248
RayhanLauzzadani <rlauzzadani@gmail.com>
2025-07-07 10:36:01 +0700

    commit nothing

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,8 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 import 'firebase_options.dart';
 import 'package:google_fonts/google_fonts.dart';
 // import 'buyer/features/home/splash_screen.dart';
-import 'package:abc_e_mart/buyer/features/home/home_page_buyer.dart';
+import 'package:abc_e_mart/buyer/features/store/store_detail_page.dart';
+// import 'package:abc_e_mart/buyer/features/home/home_page_buyer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -33,7 +34,9 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.white,
       ),
       // home: const SplashScreen(),
-      home: const HomePage(),
+      // home: const HomePage(),
+      home: const StoreDetailPage(),
+      
 
     );
   }
