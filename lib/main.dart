2eb17e30c8c90e32991a2462da8476469599a48e
ahmadnabyl <ahmadnabilassegaf7@gmail.com>
2025-07-10 00:29:21 +0700

    feat : detail alamat

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 import 'firebase_options.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'buyer/features/home/splash_screen.dart';
-import 'package:abc_e_mart/buyer/features/profile/profile_page.dart';
+// import 'package:abc_e_mart/buyer/features/profile/profile_page.dart';
 // import 'package:abc_e_mart/buyer/features/store/store_detail_page.dart';
 // import 'package:abc_e_mart/buyer/features/home/home_page_buyer.dart';
 // import 'package:abc_e_mart/seller/features/home/home_page_seller.dart';
@@ -36,12 +36,12 @@ class MyApp extends StatelessWidget {
         textTheme: GoogleFonts.dmSansTextTheme(),
         scaffoldBackgroundColor: Colors.white,
       ),
-      // home: const SplashScreen(),
-      // home: const StoreDetailPage(),
-      // home: HomePage(),
-      // home: const HomePageSeller(),
-      // home: const RegistrationWelcomePage(),
-         home: const ProfilePage(),
+        home: const SplashScreen(),
+        // home: const StoreDetailPage(),
+        // home: HomePage(),
+        // home: const HomePageSeller(),
+        //home: const RegistrationWelcomePage(),
+        //home: const ProfilePage(),
       
 
     );
