0500a07bcaf2bb317ba40a2cacbe15cf7a8fe2ad
afandiwirawan <147696851+afandiwirawan@users.noreply.github.com>
2025-07-18 18:17:43 +0700

    feat: order seller page + review order page

---
+++ lib/seller/features/order/order_tab_history.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lucide_icons/lucide_icons.dart';
+import '../../../widgets/cart_and_order_list_card.dart';
+import '../../../widgets/dummy/dummy_orders.dart';
+
+class SellerOrderTabHistory extends StatelessWidget {
+  const SellerOrderTabHistory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Dummy: riwayat order (success/canceled)
+    final orders = dummyOrdersHistory;
+
+    if (orders.isEmpty) {
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(LucideIcons.history, size: 85, color: Colors.grey[350]),
+            const SizedBox(height: 30),
+            Text(
+              "Riwayat pesanan masih kosong",
+              style: GoogleFonts.dmSans(
+                fontSize: 17,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey[700],
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              "Belum ada riwayat pesanan sebelumnya.",
+              style: GoogleFonts.dmSans(
+                fontSize: 14.5,
+                color: Colors.grey[500],
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      );
+    }
+
+    return ListView.builder(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+      itemCount: orders.length,
+      itemBuilder: (context, i) {
+        final order = orders[i];
+        return CartAndOrderListCard(
+          storeName: order.storeName, // ganti ke order.buyerName jika sudah ada
+          orderId: order.orderId,
+          productImage: order.productImage,
+          itemCount: order.itemCount,
+          totalPrice: order.totalPrice,
+          orderDateTime: order.orderDateTime,
+          status: order.status,
+          onTap: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text("Tinjau Pesanan...")),
+            );
+          },
+          onActionTap: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text("Tinjau Pesanan...")),
+            );
+          },
+          // statusText: null, // pakai label default
+        );
+      },
+    );
+  }
+}
